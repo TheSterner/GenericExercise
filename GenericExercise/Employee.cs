9c867334f25f2bd9eed25fa9ec95d1ff3528446a
William B McAllister <blakemcallister77@gmail.com>
2018-10-16 17:52:44 -0400

    pg131

---
+++ GenericExercise/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericExercise
+{
+	public class Employee<T>
+	{
+		public List<T> Things { get; set; }
+		
+
+		
+
+	}
+}
--- GenericExercise/Program.cs
+++ GenericExercise/Program.cs
@@ -6,10 +6,31 @@ using System.Threading.Tasks;
 
 namespace GenericExercise
 {
-	class Program
+	public class Program
 	{
 		static void Main(string[] args)
 		{
+			Employee<string> employee = new Employee<string>();
+			Employee<int> employeeId = new Employee<int>();
+			employee.Things = new List<string>() { "Blake", "Bob", "Barry", "Jason" };
+			employeeId.Things = new List<int>() { 5, 6, 7, 8, };
+
+			for (int i = 0; i < employee.Things.Count; i++)
+			{
+				Console.WriteLine("Name : " + employee.Things[i] + " and your Id Number is :" + employeeId.Things[i]);
+			}
+
+			Console.ReadLine();
+			//public void PrinttoConsole()
+			//{
+			//	foreach (string name in employee.Things )
+			//	{
+					
+
+
+			//	}
+			//}
+
 		}
 	}
 }
